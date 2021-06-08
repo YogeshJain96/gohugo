09b120e698575aad672e7f771512b08a9357d624
Yogesh Jain <yogesh.jain@enterprisedb.com>
2021-06-08 20:29:52 +0530

    Update Script file

--- app/src/App.js
+++ app/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Status</code> Page PoC.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://yogeshjain96.github.io/gohugo/api"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          APIs
         </a>
       </header>
     </div>
