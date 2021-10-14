58a41fe02bd2df032cb02a7ca7c3f1b3a1b2ceec
Yogesh Jain <contactyogeshjain@gmail.com>
2021-10-14 12:19:13 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+import yaml
+
+version_list = ["abc=xyz:1.2.3", "abc1=ccc:1.3.3", "abc2=pqr:4.2.3"]
+for v in version_list:
+    print("- " + v)
+    # with open(f"version_info.txt", "a") as outfile:
+    #     outfile.write("- " + v + "\n")
