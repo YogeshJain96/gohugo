d2dc5285f2fd2a4755728806d8b1b3c5e768087c
Yogesh Jain <contactyogeshjain@gmail.com>
2021-10-14 23:18:14 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 version_list = ["abc = xyz:1.2.3", "abc1 = ccc:1.3.3", "abc2 = pqr:4.2.3"]
-print(repr("\n- ".join(version_list)))
-# for v in version_list:
-#     print("\n- " + v)
-    # with open(f"version_info.txt", "a") as outfile:
-    #     outfile.write("- " + v + "\n")
+# print(repr("\n- ".join(version_list)))
+for v in version_list:
+    print("- " + v)
+    with open(f"version_info.txt", "a") as outfile:
+        outfile.write("- " + v + "\n")
